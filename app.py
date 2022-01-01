42bc1455d17c85ef38ab3ad3021f699e7a1f53bc
vahid <vahidmohsseni@yahoo.com>
2022-01-01 22:06:28 +0200

    update version 0.1

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+import Server
+
+app = Server.create_app()
+
+
+if __name__ == '__main__':
+
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
